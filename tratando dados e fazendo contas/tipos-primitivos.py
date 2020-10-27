c4c2c2edc934a86309017400483469f0931eaa6c
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-27 08:39:19 -0300

    Create tipos-primitivos.py

---
+++ tratando dados e fazendo contas/tipos-primitivos.py
@@ -0,0 +1,10 @@
+# int()
+# float()
+# bool()
+# str()
+
+n1 = int(input('Digite um valor '))
+# print(type(n1))
+n2 = int(input('Digite mais um numero '))
+s = n1 + n2
+print('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
