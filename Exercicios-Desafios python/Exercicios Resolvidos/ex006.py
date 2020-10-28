c5bf936798cfc9328501d2885676f6259ba5fb87
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-28 13:28:59 -0300

    Create ex006.py

---
+++ Exercicios-Desafios python/Exercicios Resolvidos/ex006.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+'''
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+'''
+print('O dobro de {} vale {}'.format(n, (n * 2)))
+print('O triplo de {} vale {}. \nA raiz quadrada {} é igual a {:.2f}.'.format(n, (n * 3), n, pow(n, (1/2))))
