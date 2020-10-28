65d77bd2f44ad9c08ed31cb3f780f657710710ae
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-28 13:36:03 -0300

    Update ex005.py

--- Exercicios-Desafios python/Exercicios Resolvidos/ex005.py
+++ Exercicios-Desafios python/Exercicios Resolvidos/ex005.py
@@ -1,2 +1,2 @@
 n = int(input('Digite um número: '))
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n - 1), (n + 1)))
+print(f'Analisando o valor {n}, seu antecessor é {n - 1} e o sucessor é {n + 1}')
