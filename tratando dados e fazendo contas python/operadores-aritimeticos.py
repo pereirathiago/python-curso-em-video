d43e9e5ddb53b79a7b58b24cceae3c944be10342
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-28 09:02:20 -0300

    Create operadores-aritimeticos.py

---
+++ tratando dados e fazendo contas python/operadores-aritimeticos.py
@@ -0,0 +1,16 @@
+# Operados aritimedticos
+
+# + -> adição
+#  - -> subtração
+#  * -> multiplicação
+# / -> divisão
+# ** -> potencia
+# // -> divisão inteira
+# % -> resto da divisão
+
+# Ordem de precedencia
+
+# 1° ()
+# 2° **
+# 3° * / // %
+# 4° + -
