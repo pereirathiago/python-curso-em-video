66b0e21eec81191f3f3f8bf0388a61ec5c68b870
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-28 11:55:52 -0300

    Create desafio011.py

---
+++ Exercicios-Desafios python/Desafios/desafio011.py
@@ -0,0 +1,9 @@
+print('====== DESAFIO 11 ======')
+
+h = float(input('Digite a altura: '))
+b = float(input('Digite a larura: '))
+
+a = b * h
+litros = a / 2
+
+print('Vc precisará de {} litros de tinta'.format(litros))
