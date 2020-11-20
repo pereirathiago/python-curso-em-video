56afb62665c49ebd2a02d04a92305c7e4a2a925f
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-11-20 15:23:03 -0300

    aula8

--- Mundo 1 Python/usando modulos/aula 8 - modulos.py
+++ Mundo 1 Python/usando modulos/aula 8 - modulos.py
@@ -1,2 +1,24 @@
 # import math
-# from math import pudim
+# from math import ceil
+
+# ceil -> arredonda pra cima
+# floor -> arredonda pra baixo
+# trunc -> trucamento - trunca o numero, ignora a virgula
+# pow -> potencia
+# sqrt -> raiz quadrada
+# factorial -> fatorial
+# ...
+
+# import math
+# from math import sqrt
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+
+# import random
+# # num = random.random() -> 0 até 1
+# num = random.randint(1, 10)
+# print(num)
+
+import emoji
+print(emoji.emojize("Hello world :sunglasses:", use_aliases=True))
