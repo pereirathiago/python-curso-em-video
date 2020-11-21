f7f8f6c5c9c063d3f9310e1c54d592dfcb37ef7e
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-11-21 12:47:29 -0300

    Create ex016.py

---
+++ Exercicios-Desafios python/Exercicios Resolvidos/ex016.py
@@ -0,0 +1,5 @@
+'''from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção é {}'.format(num, trunc(num)))'''
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção é {}'.format(num, int(num)))
