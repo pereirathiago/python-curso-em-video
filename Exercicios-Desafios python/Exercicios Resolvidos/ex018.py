f2f30e921b9b4ada9225cb3fc84c3b4f57b59b55
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-11-28 21:27:48 -0300

    Update ex018.py

--- Exercicios-Desafios python/Exercicios Resolvidos/ex018.py
+++ Exercicios-Desafios python/Exercicios Resolvidos/ex018.py
@@ -2,3 +2,7 @@ import math
 an = float(input('Digite o ângulo que você deseja: '))
 seno = math.sin(math.radians(an))
 print('O ângulo de {} tem o SENO de {:.2f}'.format(an, seno))
+conseno = math.cos(math.radians(an))
+print('O ângulo de {} tem o COSCENO de {:.2f}'.format(an, conseno))
+tangente = math.tan(math.radians(an))
+print('O ângulo de {} tem o TANGENTE de {:.2f}'.format(an, tangente))
