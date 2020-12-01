d596edbe45b0ab22ce10b1b9eb1cc3e0f0235570
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-12-01 14:40:06 -0300

    Create aula10.py

---
+++ Mundo 1 Python/condiçoes python(if..else)/aula10.py
@@ -0,0 +1,21 @@
+'''
+if carro.esquerda():
+    bloco _V_
+else carro.direita():
+    bloco_F_
+'''
+
+'''
+tempo = int(input('Quantos anos tem seu carro? '))
+if tempo <=3:
+    print('carro  novo')
+else:
+    print('carro velho')
+print('--fim--')
+'''
+
+'''
+tempo = int(input('Quantos anos tem seu carro? '))
+print('carro novo' if tempo <= 3 else 'carro velho')
+print('--fim--')
+'''
