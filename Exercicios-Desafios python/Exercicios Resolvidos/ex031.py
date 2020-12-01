601e1e3f14605b2cf9eda26f1ae09f494915a431
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-12-01 16:01:59 -0300

    Create ex031.py

---
+++ Exercicios-Desafios python/Exercicios Resolvidos/ex031.py
@@ -0,0 +1,8 @@
+distancia = float(input('Qual é a distancia da viagem: '))
+print('Você está prestes a começar uma viagem de {}Km'.format(distancia))
+
+if distancia <= 200:
+    preco = distancia * 0.5
+else:
+    preco = distancia * 0.45
+print('E o preço da sua passagem será de R${:.2f}'.format(preco)
