a02b59459e3066785afed9fcfd01e58a58f58506
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-12-04 08:33:09 -0300

    Update ex002.py

--- Exercicios-Desafios python/Exercicios Resolvidos/ex002.py
+++ Exercicios-Desafios python/Exercicios Resolvidos/ex002.py
@@ -1,3 +1,3 @@
-nome = input('Digite seu nome: ')
+nome = input('\033[33mDigite seu nome:\033[m ')
 # print('É um prazer te conhecer,', nome)
-print('É um prazer em te conhecer, {}!'.format(nome))
+print('É um prazer em te conhecer, \033[4;31m{}\033[m!'.format(nome))
