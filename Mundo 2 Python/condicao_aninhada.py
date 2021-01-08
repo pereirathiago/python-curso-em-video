9956de115f64633eb60d56c82aff801dddcb61f9
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-08 15:38:54 -0300

    Create condicao_aninhada.py

---
+++ Mundo 2 Python/condicao_aninhada.py
@@ -0,0 +1,14 @@
+'''
+se/senão se/senão
+if/elif/else
+'''
+nome = str(input('Qual é seu nome? '))
+if nome == 'Thiago':
+    print('Que nome bonito')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil')
+elif nome in 'Ana Claudia Jessica Juliana':
+    print('Belo nome feminino')
+else:
+    print('Seu nome é bem normal')
+print(f'Tenha um bom dia, {nome}')
